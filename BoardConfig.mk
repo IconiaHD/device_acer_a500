a92de49adf948649ec545e32b1da78c7f79fd61e
Dominique DAMBRAIN <domi@dambrain.fr>
2014-03-19 12:35:36 +0100

    Update BoardConfig.mk
    reconfigure to use prebuilt kernel ( Jelly Bean)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,9 +50,9 @@ ARCH_ARM_HIGH_OPTIMIZATION := true
 
 # kernel
 #TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
-TARGET_KERNEL_SOURCE := kernel/acer/a200
-TARGET_KERNEL_CONFIG := jellyplay_picasso_e_defconfig
-#TARGET_PREBUILT_KERNEL := device/acer/a200/prebuilt/zImage
+#TARGET_KERNEL_SOURCE := kernel/acer/a200
+#TARGET_KERNEL_CONFIG := jellyplay_picasso_e_defconfig
+TARGET_PREBUILT_KERNEL := device/acer/a200/prebuilt/zImage
 BOARD_KERNEL_CMDLINE :=
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_PAGE_SIZE := 0x00000800
