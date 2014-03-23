7b679b76fde3e34a6ec77db4301bd39dddd6c549
Henrik Enquist <henrik.enquist@gmail.com>
2014-03-23 14:03:37 +0100

    Update omni_a200.mk

--- omni_a200.mk
+++ omni_a200.mk
@@ -1,10 +1,6 @@
 # Release name
 PRODUCT_RELEASE_NAME := A200
 ROM_BUILDTYPE := IconiaHD
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/omni-4.4-hwctest
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
